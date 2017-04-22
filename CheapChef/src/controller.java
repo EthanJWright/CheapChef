9cd7d7efdc5764e1c606b5ff1d7b0658288981a4
Ethan Wright <ethanwright@Ethans-MacBook-Air.local>
2017-04-22 12:15:40 -0600

    added potato

--- CheapChef/src/controller.java
+++ CheapChef/src/controller.java
@@ -3,11 +3,15 @@ public class controller {
         System.out.println("Hello World!"); // Display the string.
         ingredients ingredientList = new ingredients();
         ingredient testIngredient = new ingredient();
+        ingredient otherItem = new ingredient();
         testIngredient.setIngredient("carrot");
+        otherItem.setIngredient("potato");
         ingredientList.addIngredient(testIngredient);
-        ingredientList.addIngredient(testIngredient);
+        ingredientList.addIngredient(otherItem);
         ingredient carrot = ingredientList.getIngredients().get(0);
+        ingredient potato = ingredientList.getIngredients().get(1);
         System.out.println(carrot.getIngredient());
+        System.out.println(potato.getIngredient());
         
     }
 
