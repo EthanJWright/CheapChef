83741797a04a2a891ef4564350781870b813402f
Ethan Wright <ethanwright@Ethans-MacBook-Air.local>
2017-04-22 12:18:44 -0600

    Adding comments and spacing

--- CheapChef/src/controller.java
+++ CheapChef/src/controller.java
@@ -2,14 +2,24 @@ public class controller {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
         ingredients ingredientList = new ingredients();
+        
+        // Make some ingredients to add
         ingredient testIngredient = new ingredient();
         ingredient otherItem = new ingredient();
+        
+        // name the ingredients
         testIngredient.setIngredient("carrot");
         otherItem.setIngredient("potato");
+        
+        // put them both into our list of ingredients
         ingredientList.addIngredient(testIngredient);
         ingredientList.addIngredient(otherItem);
+        
+        // get ingredients from our list of ingredients
         ingredient carrot = ingredientList.getIngredients().get(0);
         ingredient potato = ingredientList.getIngredients().get(1);
+        
+        // print the ingredients
         System.out.println(carrot.getIngredient());
         System.out.println(potato.getIngredient());
         
