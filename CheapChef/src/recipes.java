57ab0e5f19e2af034c84bc65fcd6cf9a3bca8fb5
Julio Lemos <jule3935@colorado.edu>
2017-04-22 13:58:03 -0600

    adding the work i have done in recipe and recipes

--- CheapChef/src/recipes.java
+++ CheapChef/src/recipes.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
 
 public class recipes {
+	ArrayList<recipe> databaseRecipes = new ArrayList<recipe>();
+	ArrayList<recipe> userRecipes = new ArrayList<recipe>();
+	public int price;
+	public ArrayList<recipe> getDatabaseRecipes() {
+		return databaseRecipes;
+	}
+	public void setDatabaseRecipes(ArrayList<recipe> databaseRecipes) {
+		this.databaseRecipes = databaseRecipes;
+	}
+	public ArrayList<recipe> getUserRecipes() {
+		return userRecipes;
+	}
+	public void setUserRecipes(ArrayList<recipe> userRecipes) {
+		this.userRecipes = userRecipes;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	 
+	
 
 }
