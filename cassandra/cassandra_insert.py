d07884cb3f13ec595e313543617aea1e352d898e
Ethan Wright <ethanwright@engr2-12-87-dhcp.int.colorado.edu>
2017-04-24 12:40:34 -0600

    Set up new ingredient database which includes categories

--- cassandra/cassandra_insert.py
+++ cassandra/cassandra_insert.py
@@ -4,18 +4,25 @@ from cassandra.cluster import Cluster
 with open('ingredients.json') as data_file:
         data = json.load(data_file)
 
-        names = data.keys()
-        costs = data.values()
-        restricted = [False] * len(costs)
-
-
 cluster = Cluster()
 session = cluster.connect('cheapchef')
-for i in range(0, len(names)):
-    session.execute(
-            """
-            INSERT INTO ingredients (ingredient, ingredient_name, ingredient_price, ingredient_restricted)
-            VALUES (%s, %s, %s, %s)
-            """,
-            (i, names[i], int(costs[i]), restricted[i])
-            )
+category = data.keys()
+index = 0
+for i in range(0, len(category)-1):
+    names = data[category[i]].keys()
+    price = data[category[i]].values()
+    for j in range(0, len(names)-1):
+        index += 1
+        ingredient_name = names[j]
+        ingredient_price = price[j]
+        ingredient_category = category[i]
+
+        print names[j], ' costs: ', price[j]
+
+        session.execute(
+                """
+                INSERT INTO ingredients (ingredient, ingredient_name, ingredient_price, category)
+                VALUES (%s, %s, %s, %s)
+                """,
+                (index, ingredient_name, int(ingredient_price), ingredient_category)
+                )
--- cassandra/json_test.py
+++ cassandra/json_test.py
@@ -3,9 +3,14 @@ import json
 with open('ingredients.json') as data_file:
         data = json.load(data_file)
 
-        names = data.keys()
-        costs = data.values()
-        restrict = [False] * len(costs)
+category = data.keys()
+for i in range(0, len(category)-1):
+    names = data[category[i]].keys()
+    price = data[category[i]].values()
+    for j in range(0, len(names)-1):
+        print names[j], ' costs: ', price[j], ' in category: ', category[i]
 
-        print str(names[0]), ' costs: ', int(costs[0])
-        print int(costs[0]) + 1
+#        print costs
+
+#        print str(names[0]), ' costs: ', int(costs[0])
+#        print int(costs[0]) + 1
