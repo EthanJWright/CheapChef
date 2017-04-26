ee980683482cd06fc1710a2d5ca8e4f49edcfef1
Ethan Wright <ethanwright@engr2-15-214-dhcp.int.colorado.edu>
2017-04-26 11:45:16 -0600

    added meat to ingredients as well and renamed for consistency

--- cassandra/cassandra_ingredients.py
+++ cassandra/cassandra_ingredients.py
@@ -8,7 +8,7 @@ cluster = Cluster()
 session = cluster.connect('cheapchef')
 category = data.keys()
 index = 0
-for i in range(0, len(category)-1):
+for i in range(0, len(category)):
     names = data[category[i]].keys()
     price = data[category[i]].values()
     for j in range(0, len(names)-1):
@@ -21,7 +21,7 @@ for i in range(0, len(category)-1):
 
         session.execute(
                 """
-                INSERT INTO ingredients_sortable (ingredient, ingredient_name, ingredient_price, category)
+                INSERT INTO ingredients (ingredient, ingredient_name, ingredient_price, category)
                 VALUES (%s, %s, %s, %s)
                 """,
                 (index, ingredient_name, int(ingredient_price), ingredient_category)
