a657e8518993a7e665df7812f7956a25f2ab109c
petarthepirate <nhannguyen@engr2-1-83-dhcp.int.colorado.edu>
2017-04-30 16:35:14 -0600

    added recipe.java

---
+++ CheapChef/src/recipe.java
@@ -0,0 +1,21 @@
+
+public class recipe {
+	
+	ingredients recipeIngredients = new ingredients();
+	public String recipeName;
+	
+	public ingredients getRecipeIngredients() {
+		return recipeIngredients;
+	}
+	public void setRecipeIngredients(ingredients recipeIngredients) {
+		this.recipeIngredients = recipeIngredients;
+	}
+	public String getRecipeName() {
+		return recipeName;
+	}
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+	
+
+}
